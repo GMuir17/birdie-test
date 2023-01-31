feeb3812c850c64d66b3bbab4515079f32b8cf36
Gary <garymuir17@gmail.com>
2023-01-31 23:26:25 +0000

    debugging test

--- backend/__tests__/events.spec.ts
+++ backend/__tests__/events.spec.ts
@@ -7,14 +7,20 @@ dotenv.config();
 describe("Get /events", () => {
   console.log("banana in the test 1", process.env.DB_PORT);
   it("should return at least one object with caregiver_id", async () => {
-    console.log("banana in the test 2");
+    console.log("banana in the test 2", process.env.super_secret);
 
     await request(app)
-      .get("/events")
+      .get("/hello")
       .expect(200)
       .expect(function (res) {
-        expect(res.body.events[0]["caregiver_id"]).toBeTruthy();
+        expect(res.body.greetings).toContain("Thank you");
       });
+    // await request(app)
+    //   .get("/events")
+    //   .expect(200)
+    //   .expect(function (res) {
+    //     expect(res.body.events[0]["caregiver_id"]).toBeTruthy();
+    //   });
     console.log("banana in the test 3");
   });
 });
