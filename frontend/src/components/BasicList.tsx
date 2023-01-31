2629e28ee01f9b2e723febb8e8c63bc7d96fb433
Gary <garymuir17@gmail.com>
2023-01-31 21:41:34 +0000

    basic front end test with react testing library and jest

--- frontend/src/components/BasicList.tsx
+++ frontend/src/components/BasicList.tsx
@@ -1,31 +1,57 @@
-import { useEffect } from "react";
-import { Typography, Box } from "@mui/material";
-import { useQuery } from "react-query";
-import axios from "axios";
+import { useState } from "react";
+import {
+  Typography,
+  Box,
+  FormControl,
+  MenuItem,
+  InputLabel,
+} from "@mui/material";
+// import { useQuery } from "react-query";
+// import axios from "axios";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
 
 const BasicList = () => {
-  const { data, isLoading } = useQuery(
-    `basic-test`,
-    async () => {
-      const res = await axios.get(`http://localhost:8000/events`);
-      console.log("banana data", res.data);
-      return res.data;
-    },
-    { staleTime: 60000 }
-  );
+  const [age, setAge] = useState("10");
 
-  useEffect(() => {
-    console.log("banana data", { data });
-  }, [data]);
+  // const { data, isLoading } = useQuery(
+  //   `basic-test`,
+  //   async () => {
+  //     const res = await axios.get(`http://localhost:8000/events`);
+  //     console.log("banana data", res.data);
+  //     return res.data;
+  //   },
+  //   { staleTime: 60000 }
+  // );
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setAge(event.target.value as string);
+  };
 
   return (
     <Box sx={{ my: 4 }}>
       <Typography variant="h4" component="h1" gutterBottom>
         Basic list of events
       </Typography>
-      {isLoading && <div>Loading...</div>}
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          value={age}
+          label="Age"
+          onChange={handleChange}
+          inputProps={{ "data-testid": "select-input" }}
+          SelectDisplayProps={{ "data-testid": "select-button" } as {}}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem id="select-button" value={20}>
+            Twenty
+          </MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+      </FormControl>
+      {/* {isLoading && <div>Loading...</div>}
       {data &&
-        data.events.map((event: any) => <div key={event.id}>{event.id}</div>)}
+        data.events.map((event: any) => <div key={event.id}>{event.id}</div>)} */}
     </Box>
   );
 };
