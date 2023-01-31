7ae753db3a4ad306aaac704f9139c92b7a716f37
Gary <garymuir17@gmail.com>
2023-01-31 23:46:39 +0000

    gets backend test running on github action

--- backend/__tests__/events.spec.ts
+++ backend/__tests__/events.spec.ts
@@ -5,15 +5,12 @@ import app from "../src/application";
 dotenv.config();
 
 describe("Get /events", () => {
-  console.log("banana in the test 1", process.env.DB_PORT);
   it("should return at least one object with caregiver_id", async () => {
-    console.log("banana in the test 2", process.env.DB_NAME);
     await request(app)
       .get("/events")
       .expect(200)
       .expect(function (res) {
         expect(res.body.events[0]["caregiver_id"]).toBeTruthy();
       });
-    console.log("banana in the test 3");
   });
 });
