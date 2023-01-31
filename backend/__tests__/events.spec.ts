b65c84c9ce9f990b8a545bea07846f58145a0de3
Gary <garymuir17@gmail.com>
2023-01-31 23:20:37 +0000

    debugging test

--- backend/__tests__/events.spec.ts
+++ backend/__tests__/events.spec.ts
@@ -5,7 +5,7 @@ import app from "../src/application";
 dotenv.config();
 
 describe("Get /events", () => {
-  console.log("banana in the test 1");
+  console.log("banana in the test 1", process.env.DB_PORT);
   it("should return at least one object with caregiver_id", async () => {
     console.log("banana in the test 2");
 
